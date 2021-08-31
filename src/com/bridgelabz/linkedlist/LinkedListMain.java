8472200e2b2eb3d56d5ff6eca99c6cdb08a2d69d
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-01 02:37:14 +0530

    Added: To create a simple linked list

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -1,8 +1,19 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedListMain {
-
+	
 	public static void main(String[] args) {
-
+		
+		Node<Integer> firstNode = new Node<>();
+		firstNode.setKey(56);
+		Node<Integer> secondNode = new Node<>();
+		secondNode.setKey(30);
+		Node<Integer> thirdNode = new Node<>();
+		thirdNode.setKey(70);
+		
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		
+		new LinkedList<Integer>().printLinkedList(firstNode);
 	}
 }
